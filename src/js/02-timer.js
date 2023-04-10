9b53ba9f95a5e6b1d319d03b47906d02288fcd9f
UriyRatinskiy <uriy_mark@ukr.net>
2023-04-10 22:12:26 +0300

    HW09-04

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,28 +1,29 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
 const refs = {
-    timerInput: document.querySelector('#datetime-picker'),
-    btnStart: document.querySelector('button[data-start]'),
-    day: document.querySelector('span.value[data-days]'),
-    hour: document.querySelector('span.value[data-hours]'),
-    minute: document.querySelector('span.value[data-minutes]'),
-    second: document.querySelector('span.value[data-seconds]'),
+  timerInput: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+  day: document.querySelector('span.value[data-days]'),
+  hour: document.querySelector('span.value[data-hours]'),
+  minute: document.querySelector('span.value[data-minutes]'),
+  second: document.querySelector('span.value[data-seconds]'),
 };
-console.log(refs);
 
-refs.btnStart.addEventListener('click', onBtnStart),
+refs.btnStart.addEventListener('click', onBtnStart)
+
 refs.btnStart.disabled = true;
 
 let futureTime = null;
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      futureTime = selectedDates[0].getTime();
+        futureTime = selectedDates[0].getTime();
 
         if (selectedDates[0] <= options.defaultDate) {
             refs.btnStart.disabled = true;
@@ -32,29 +33,30 @@ const options = {
             return futureTime;
         }
     },
-  };
-  const fp = flatpickr(refs.timerInput, options)
+};
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  function addLeadingZero(value) {
+const fp = flatpickr(refs.timerInput, options)
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 }
 
